04dc999f53c47c90b123f11944a1574536ef425b
unknown <coutinhnicolasgabriel@gmail.com>
2025-05-13 14:27:13 -0300

    Arrumei o problema que acontecia se o usuário não digitasse um número

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 let notas = 0;
+
 let soma = 0;
+
 let i = 0;
 
 do {
 
     notas = parseFloat(prompt("Insira sua nota:"));
-    i++;
-    soma += notas;
 
-    if (isNaN(notas) || notas < 0 || notas > 10) {
+    if (notas >= 0 && notas <= 10) {
 
-        i--;
-        soma -= notas;
+        i++;
+        soma += notas;
 
     }
 
-} while (notas >= 0 && notas <= 10);
+} while (notas >= 0 && notas <= 10 || isNaN(notas));
 
 alert(`A média das suas notas é ${soma / i}`)
