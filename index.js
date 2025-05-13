9e2a4ae87b85f3c5e88c54f5e4ad3833e0eeab1b
unknown <coutinhnicolasgabriel@gmail.com>
2025-05-13 13:12:19 -0300

    Criado um algoritmo para armazenar o nome do usuário

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-let compra = 0;
-let total = 0;
+let nome = 0;
+let sobrenome = 0;
 
-do {
+while (nome >= 0 || nome<=0) {
 
-    compra = parseFloat(prompt("Insira o valor da compra:"));
+    nome = prompt("Digite seu nome:")
+
+}
+
+  while (sobrenome>=0 || sobrenome<=0) {
+
+    sobrenome = prompt("Digite seu sobrenome:")
     
-    if(isNaN(compra)){
-        
-        alert("Erro: Você não digitou um número válido!");
-        
-    }else {
-        
-        total += compra;
-        
-    }
+  }  
 
-} while (compra != 0);
-
-alert(`O valor da sua compra é de R$ ${total}!`)
+alert(`O seu nome é ${nome} ${sobrenome}`);
